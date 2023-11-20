0fe84c04b0eaf16307946ff4b2a5b97e26d102eb
Schwi <39186981+cyb233@users.noreply.github.com>
2023-11-20 13:08:55 +0800

    Update bili.user.js

--- tampermonkey/bilibili/bili.user.js
+++ tampermonkey/bilibili/bili.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         颜色标识B站收藏番剧作品更新状态
 // @namespace    Schwi
-// @version      0.1
+// @version      0.2
 // @description  灰色未开播，绿色已完结，红色连载中
 // @author       Schwi
 // @match        https://space.bilibili.com/*/bangumi
@@ -15,6 +15,17 @@
     const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
     async function repeatLoop() {
         while (true) {
+            const tips = document.querySelector("#tips")
+            if (tips == null) {
+                const tabs = document.querySelector(".sub-tabs.clearfix")
+                const filter = document.querySelector(".sub-tabs.clearfix > .filter-content")
+                const span = document.createElement('span')
+                span.id = 'tips'
+                span.innerHTML = '<span style="background:#00000020">即将开播</span> <span style="background:#00ff0020">已完结</span> <span style="background:#ff000020">连载中</span>'
+                if (tabs && filter) {
+                    tabs.insertBefore(span, filter)
+                }
+            }
             const follows = document.querySelectorAll(".pgc-follow-list > .pgc-space-follow-item")
             follows.forEach(ele=>{
                 const state = ele.querySelector('.publish-state')
